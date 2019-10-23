b3ddf13252d737e0294d67c14a99ac3cf250a0b0
torianne02 <victoria.fluharty@gmail.com>
2019-10-23 10:29:21 -0700

    creates separate class to make API call adds logic to call api in batches - 50 at a time up until 1000 words

---
+++ lib/WordCall.rb
@@ -0,0 +1,37 @@
+require 'httparty'
+
+class Hangman::WordCall 
+
+  def initialize
+    @words = []
+
+    get_words_arr
+  end 
+
+  def get_words_arr
+    start_count = 0
+
+    keep_fetching = true
+
+    while keep_fetching
+      api_response = HTTParty.get("http://app.linkedin-reach.io/words?start=#{start_count}&count=50")
+
+      if api_response.code != 200
+        raise "#{response.code}: Failed to call LinkedIn API"
+      else 
+        response_arr = api_response.parsed_response.split
+        response_arr.map {|word| @words << word}
+
+        if response_arr.length < 50 || @words.length >= 1000
+          keep_fetching = false
+        else 
+          start_count += 50
+        end 
+      end
+    end 
+
+    def word 
+      @words.sample
+    end 
+  end
+end 
